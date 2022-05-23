108a1f5214572a3fca0bfce1fcef1c3373416ecd
Krzysztof Szuba <krzysiu.sz120@gmail.com>
2022-05-23 22:13:56 +0200

    Refactor

--- src/main/java/io/github/kszuba1/MyCoursesApplication.java
+++ src/main/java/io/github/kszuba1/MyCoursesApplication.java
@@ -1,9 +1,12 @@
 package io.github.kszuba1;
 
+import io.github.kszuba1.dao.AccountRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = AccountRepository.class)
 public class MyCoursesApplication {
 
 	public static void main(String[] args) {
