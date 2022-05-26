d0eab59c3f6522e49c7d62a4fdbd2625f1def719
Krzysztof Szuba <krzysiu.sz120@gmail.com>
2022-05-27 00:06:24 +0200

    Add @Documented annotation

--- src/main/java/io/github/kszuba1/validation/Nickname.java
+++ src/main/java/io/github/kszuba1/validation/Nickname.java
@@ -2,11 +2,9 @@ package io.github.kszuba1.validation;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Constraint(validatedBy = NicknameConstraintValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD } )
 @Retention(RetentionPolicy.RUNTIME)
